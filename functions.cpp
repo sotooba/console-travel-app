0f8861b8207ba6ac6d6004b771e8d7c82b01cc5b
Tooba <toobasoomro27@gmail.com>
2025-11-21 21:13:28 +0500

    Add Currency sign before price.

--- functions.cpp
+++ functions.cpp
@@ -36,12 +36,14 @@ void viewPackages()
     cout << left << setw(5) << "ID" << setw(15) << "Destination" << setw(15) << "Cost/Person" << endl;
     cout << "----------------------------------------\n";
 
-    cout << left << setw(5) << 1 << setw(15) << "Islamabad" << setw(15) << 6000 << endl;     
-    cout << left << setw(5) << 2 << setw(15) << "Peshawar" << setw(15) << 8000 << endl;     
-    cout << left << setw(5) << 3 << setw(15) << "Karachi" << setw(15) << 5000 << endl;     
-    cout << left << setw(5) << 4 << setw(15) << "Lahore" << setw(15) << 7000 << endl;     
-    cout << left << setw(5) << 5 << setw(15) << "Quetta" << setw(15) << 6000 << endl;     
-    cout << left << setw(5) << 6 << setw(15) << "Multan" << setw(15) << 7000 << endl;     
+    cout << left << setw(5) << 1 << setw(15) << "Islamabad" << "Rs. "  << setw(15) << 6000 << endl;     
+    cout << left << setw(5) << 2 << setw(15) << "Peshawar" << "Rs. "  << setw(15) << 8000 << endl;     
+    cout << left << setw(5) << 3 << setw(15) << "Faislabad" << "Rs. "  << setw(15) << 6500 << endl;     
+    cout << left << setw(5) << 4 << setw(15) << "Karachi" << "Rs. "  << setw(15) << 5000 << endl;     
+    cout << left << setw(5) << 5 << setw(15) << "Gilgit" << "Rs. "  << setw(15) << 4000 << endl;     
+    cout << left << setw(5) << 6 << setw(15) << "Multan" << "Rs. "  << setw(15) << 6500 << endl;     
+    cout << left << setw(5) << 7 << setw(15) <<"Quetta"  << "Rs. "  << setw(15) << 7000 << endl;     
+    cout << left << setw(5) << 8 << setw(15) << "Lahore" << "Rs. "  << setw(15) << 7500 << endl;     
 
     cout << "----------------------------------------\n";
 }
